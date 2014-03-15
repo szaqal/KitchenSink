7e9651ffc9fc582e07f7f9e607d447221f67e0d9
szaqal <pmalczyk@akamai.com>
2014-03-15 20:56:29 +0100

    guava version update

--- Java/Guava/szaqalguava/src/test/java/szaqal/guava/test/TestHashing.java
+++ Java/Guava/szaqalguava/src/test/java/szaqal/guava/test/TestHashing.java
@@ -8,6 +8,8 @@ import com.google.common.hash.HashCode;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
+import java.nio.charset.Charset;
+
 /**
  * @author malczyk.pawel@gmail.com
  *
@@ -17,15 +19,15 @@ public class TestHashing {
 	@Test
 	public void testMd5() {
 		HashFunction md5 = Hashing.md5();
-		HashCode hashString = md5.hashString("BeforeHash");
-		assertEquals("07eb52c22edb599f5c18201c40cd23cb", hashString.toString());
+		HashCode hashString = md5.hashString("BeforeHash", Charset.defaultCharset());
+		assertEquals("86b3a7bd44852cf9cc897e262ccb4edd", hashString.toString());
 	}
 	
 	@Test
 	public void testSha512() {
 		HashFunction sha512 = Hashing.sha512();
-		HashCode hashString = sha512.hashString("BeforeHash");
-		assertEquals("5c399c789482907b556b07ee383907bf6ea368b497b22949ba78e3969e06151815ae7adc59686057fc8c961cdc869bf84c1bdbf8a96d69e1b15ebc24bb60b8c6", hashString.toString());
+		HashCode hashString = sha512.hashString("BeforeHash", Charset.defaultCharset());
+		assertEquals("12ccc1ec7265c18840ab0b5b70b8b776fca4015f3ed62eb0d2c2bf727fe8e108b77e9f49458e47e98d852861b8a0bb83a9b5dd4fe84f89ce3d5fe6623142426f", hashString.toString());
 	}
 	
 }
