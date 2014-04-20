c84981a42a9d0784a0824d282c1236af72d935f5
szaqal <malczyk.pawel@gmail.com>
2014-04-20 11:06:33 +0200

    Minor improvemnents

--- Java/Concurrent/concurrency/src/main/java/szaq/concurrent/dataset/FileProcessor.java
+++ Java/Concurrent/concurrency/src/main/java/szaq/concurrent/dataset/FileProcessor.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -19,9 +22,9 @@ public class FileProcessor {
 
     private static final Logger LOG = LoggerFactory.getLogger(FileProcessor.class);
 
-    public static void processSingleThread() {
+    public static Map<String, Integer> processSingleThread() {
         boolean jobDone = false;
-        List<Future<?>> tasks = new ArrayList<>();
+        List<Future<Map<String, Integer>>> tasks = new ArrayList<>();
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         String line;
         try {
@@ -31,7 +34,7 @@ public class FileProcessor {
             while ((line = reader.readLine()) != null) {
                 count++;
                 dataToProcess.add(line);
-                if (count % 100 == 0) {
+                if (count % 1000 == 0) {
                     tasks.add(executorService.submit(new ComputeData(dataToProcess)));
                     dataToProcess = new ArrayList<>();
                     count = 0;
@@ -46,9 +49,9 @@ public class FileProcessor {
 
         LOG.debug("Tasks size {} ", tasks.size());
 
-        while(!jobDone) {
-            for(Future<?> task : tasks) {
-                if(!task.isDone()) {
+        while (!jobDone) {
+            for (Future<?> task : tasks) {
+                if (!task.isDone()) {
                     continue;
                 }
             }
@@ -56,14 +59,14 @@ public class FileProcessor {
 
         }
 
-
+        return aggregateData(tasks);
     }
 
 
-    public static void processMultitThread() {
+    public static Map<String, Integer> processMultitThread(int threadCount) {
         boolean jobDone = false;
-        List<Future<?>> tasks = new ArrayList<>();
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        List<Future<Map<String, Integer>>> tasks = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         String line;
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(FileProcessor.class.getClassLoader().getResourceAsStream("sample_dataset.csv")));
@@ -87,16 +90,42 @@ public class FileProcessor {
 
         LOG.debug("Tasks size {} ", tasks.size());
 
-        while(!jobDone) {
-            for(Future<?> task : tasks) {
-                if(!task.isDone()) {
+
+        while (!jobDone) {
+            for (Future<?> task : tasks) {
+                if (!task.isDone()) {
                     continue;
                 }
             }
             jobDone = true;
+        }
 
+       return aggregateData(tasks);
+
+
+    }
+
+    private static  Map<String, Integer> aggregateData(List<Future<Map<String, Integer>>> tasks) {
+        Map<String, Integer> aggregatedResults = new HashMap<>();
+        for (Future<Map<String, Integer>> task : tasks) {
+            try {
+                Map<String, Integer> taskResut = task.get();
+                for (String key : taskResut.keySet()) {
+
+                    if (aggregatedResults.containsKey(key)) {
+                        aggregatedResults.put(key, aggregatedResults.get(key) + taskResut.get(key));
+                    } else {
+                        aggregatedResults.put(key, taskResut.get(key));
+                    }
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
         }
 
 
+
+        LOG.debug("{}", aggregatedResults);
+        return aggregatedResults;
     }
 }
--- Java/Concurrent/concurrency/src/test/java/szaq/concurrent/dummyincrementdecrement/DatasetTest.java
+++ Java/Concurrent/concurrency/src/test/java/szaq/concurrent/dummyincrementdecrement/DatasetTest.java
@@ -5,20 +5,42 @@ import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import org.junit.Test;
 import szaq.concurrent.dataset.FileProcessor;
 
+import java.util.Map;
+import java.util.Random;
+
+import static junit.framework.Assert.assertEquals;
+
+
 /**
  * Created by pmalczyk on 4/19/14.
  */
 public class DatasetTest extends AbstractBenchmark {
 
-    @BenchmarkOptions(benchmarkRounds = 20)
+    @BenchmarkOptions(benchmarkRounds = 100)
     @Test
     public void testSingleThread() {
-        FileProcessor.processSingleThread();
+        Map<String,Integer> aggregatedResults = FileProcessor.processSingleThread();
+        assertEquals(Integer.valueOf(3), aggregatedResults.get("SHARON MARIE"));
     }
 
-    @BenchmarkOptions(benchmarkRounds = 20)
+    @BenchmarkOptions(benchmarkRounds = 100)
     @Test
-    public void testMultiThread() {
-        FileProcessor.processMultitThread();
+    public void testMultiThread2Threads() {
+        Map<String,Integer> aggregatedResults = FileProcessor.processMultitThread(2);
+        assertEquals(Integer.valueOf(3), aggregatedResults.get("SHARON MARIE"));
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 100)
+    @Test
+    public void testMultiThread4Threads() {
+        Map<String,Integer> aggregatedResults = FileProcessor.processMultitThread(4);
+        assertEquals(Integer.valueOf(3), aggregatedResults.get("SHARON MARIE"));
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 100)
+    @Test
+    public void testMultiThread6Threads() {
+        Map<String,Integer> aggregatedResults = FileProcessor.processMultitThread(6);
+        assertEquals(Integer.valueOf(3), aggregatedResults.get("SHARON MARIE"));
     }
 }
