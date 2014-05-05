0ef3357e4e7f3ec7c450148c195a00890f6d983b
szaqal <malczyk.pawel@gmail.com>
2014-05-05 15:52:17 +0200

    More problems

--- Scala/misc_problems/src/main/scala/szaq/problems/list/ListProblems.scala
+++ Scala/misc_problems/src/main/scala/szaq/problems/list/ListProblems.scala
@@ -24,4 +24,22 @@ object ListProblems {
     case _ => throw new NoSuchElementException
   }
 
+  def nthElement[T](which: Int, arg: List[T]): T = (which, arg) match {
+    case (0, head :: _) => head
+    case (which, head :: _) => nthElement(which - 1, arg.tail)
+    case _ => throw new NoSuchElementException
+  }
+
+  def length[T](arg: List[T]): Int = arg match {
+    case Nil => 0
+    case head :: tail => length(tail) + 1
+    case _ => 0
+  }
+  
+  def reverse[T](arg: List[T]):List[T] = arg match {
+    case Nil => List()
+    case head :: tail => reverse(tail) ::: List(head)
+    case _ => List();
+  }
+
 }
--- Scala/misc_problems/src/test/scala/szaq/problems/test/list/ListProblemsTest.scala
+++ Scala/misc_problems/src/test/scala/szaq/problems/test/list/ListProblemsTest.scala
@@ -63,4 +63,49 @@ class ListProblemsTest extends FunSuite {
     }
   }
 
+  //----
+
+  test("nth1") {
+    assert(ListProblems.nthElement(2, List(1, 2, 3)) === 3);
+  }
+
+  test("nth2") {
+    intercept[java.util.NoSuchElementException] {
+      ListProblems.nthElement(2, List[Int](1))
+    }
+  }
+
+  test("nth3") {
+    intercept[java.util.NoSuchElementException] {
+      ListProblems.nthElement(2, List[Int](0))
+    }
+  }
+
+  // ----
+
+  test("length1") {
+    assert(ListProblems.length(List(1, 2, 3)) === 3);
+  }
+
+  test("length2") {
+    assert(ListProblems.length(List(1)) === 1)
+  }
+
+  test("length3") {
+    assert(ListProblems.length(List()) === 0)
+  }
+
+  // ----
+
+  test("rev1") {
+    assert(ListProblems.reverse(List(1, 2, 3)) === List(3,2,1));
+  }
+
+  test("rev2") {
+    assert(ListProblems.reverse(List(1)) === List(1))
+  }
+
+  test("rev3") {
+    assert(ListProblems.reverse(List()) === List())
+  }
 }
