f5c5ddc9d6df323a28e9396fa27c0f9d8aa42670
szaqal <malczyk.pawel@gmail.com>
2014-07-13 21:40:00 +0200

    Shiny

---
+++ R/shiny/01/ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Data science FTW!"),
+  sidebarPanel(
+    numericInput('id1', 'Numeric input, labeled id1', 0, min = 0, max = 10, step = 1),
+    checkboxGroupInput("id2", "Checkbox",
+                       c("Value 1" = "1",
+                         "Value 2" = "2",
+                         "Value 3" = "3")),
+    dateInput("date", "Date:") 
+    
+  ),
+  mainPanel(
+    h3('Main Panel text'),
+    code('some code'),
+    p('some ordinary text'),
+    
+    h3('Illustrating outputs'),
+    h4('You entered'),
+    verbatimTextOutput("oid1"),
+    h4('You entered'),
+    verbatimTextOutput("oid2"),
+    h4('You entered'),
+    verbatimTextOutput("odate")
+  )
+))
