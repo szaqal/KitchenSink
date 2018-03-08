f04ccdc6190651232a6e71aed6d0ab01f44a9073
szaqal <malczyk.pawel@gmail.com>
2018-03-08 14:32:11 +0100

    Import

--- Java/DemoFirebase/src/main/java/szaq/App.java
+++ Java/DemoFirebase/src/main/java/szaq/App.java
@@ -1,13 +1,33 @@
 package szaq;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		String config = "{}";
+
+		InputStream serviceAccount = new ByteArrayInputStream(config.getBytes());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setDatabaseUrl("https://test-adb3b.firebaseio.com/").build();
+		FirebaseApp.initializeApp(options);
+		System.out.println("App initialized ");
+		
+		final FirebaseDatabase database = FirebaseDatabase.getInstance();
+		DatabaseReference ref = database.getReference();
+		DatabaseReference dataRef = ref.child("key");
+		
+		
+		dataRef.setValueAsync("val").get();
+		System.out.println("Here");
+	}
 }
