3f0ea6d1e960898184296ac888ab8f788e8d991b
szaqal <malczyk.pawel@gmail.com>
2019-03-21 14:48:00 +0100

    Go

---
+++ Go/Channels/Sync/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func work(done chan bool) {
+	fmt.Println("work..")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go work(done)
+	<-done
+
+}
