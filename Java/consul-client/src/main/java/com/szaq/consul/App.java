bb7b72e57dec784b330bf94c2742552e35df702b
szaqal <malczyk.pawel@gmail.com>
2020-02-10 08:52:07 +0100

    Consul client

--- Java/consul-client/src/main/java/com/szaq/consul/App.java
+++ Java/consul-client/src/main/java/com/szaq/consul/App.java
@@ -1,13 +1,29 @@
 package com.szaq.consul;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.KeyValueClient;
+import com.orbitz.consul.cache.KVCache;
+
+public class App {
+
+  private static final Logger log = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) throws InterruptedException {
+    Consul client = Consul.builder().build();
+    log.info("client {}", client);
+    KeyValueClient kvClient = client.keyValueClient();
+    KVCache cache = KVCache.newCache(kvClient, "localdev");
+    cache.addListener(map -> {
+      log.info("Cache changed");
+      map.entrySet().forEach(x->log.info("{} {}", x.getKey(), x.getValue()));
+    });
+
+    cache.start();
+
+    Thread.sleep(100_000);
+  }
 }
