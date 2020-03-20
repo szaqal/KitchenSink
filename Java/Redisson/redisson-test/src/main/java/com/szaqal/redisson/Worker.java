fdfcb3c40b47c848a17baec6b0939b449acc54f5
szaqal <malczyk.pawel@gmail.com>
2020-03-20 11:52:20 +0100

    Redisson

--- Java/Redisson/redisson-test/src/main/java/com/szaqal/redisson/Worker.java
+++ Java/Redisson/redisson-test/src/main/java/com/szaqal/redisson/Worker.java
@@ -1,10 +1,12 @@
 package com.szaqal.redisson;
 
 import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -33,25 +35,34 @@ public class Worker implements Runnable {
     public void run() {
         int keyCount =0;
         long jobStart = System.currentTimeMillis();
+        RMap<Object, Object> map = redissonClient.getMap(UUID.randomUUID().toString());
+
         for (int i = 0; i < Defaults.iterationsCount(); i++) {
             String key = UUID.randomUUID().toString();
+
+            byte[] generate = generate();
             RBucket<byte[]> bucket = redissonClient.getBucket(key);
             if (!bucket.isExists()) {
-                bucket.set(generate(), 15, TimeUnit.SECONDS);
+
+                bucket.set(generate, 15, TimeUnit.SECONDS);
                 byte[] andDelete = bucket.get();
                 LOG.trace("{}",andDelete.length);
             }
 
-            if (i%250==0) {
-
-                for(String keyExistng :redissonClient.getKeys().getKeys()) {
-
-                    keyCount++;
-                }
-                LOG.info("{}", i);
+            if(!map.isExists()) {
+                map.fastPutIfAbsent(key, generate);
             }
         }
 
+
+        // --- HEAVY STUFF
+        for(String keyExistng :redissonClient.getKeys().getKeys()) {
+            keyCount++;
+        }
+
+        Map<Object, Object> all = map.getAll(map.readAllKeySet());
+        LOG.info("MAP size :[{}]", all.keySet().size());
+        map.deleteAsync();
 
         LOG.info("[{}] DONE in [{}] [{}] ms / [{}] keys", toString(), System.currentTimeMillis() - start, System.currentTimeMillis() - jobStart, keyCount);
     }
