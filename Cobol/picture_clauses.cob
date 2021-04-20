770e34071c7306ffa893570099f3ddd48625de68
szaqal <malczyk.pawel@gmail.com>
2021-04-20 15:41:24 +0200

    Cobol

--- Cobol/picture_clauses.cob
+++ Cobol/picture_clauses.cob
@@ -11,6 +11,7 @@
        77 SOME-VALUE-4                PIC 9(3)             VALUE 1000.
        77 SOME-VALUE-5                PIC 9(4)             VALUE 1000.
        77 SOME-VALUE-6                PIC +Z,ZZZ,ZZ9.99    VALUE 100000.
+       77 SOME-VALUE-7                PIC Z,ZZZ,ZZ9.99    VALUE 100000.
        PROCEDURE DIVISION.          
            DISPLAY "SOME-VALUE-1:   "  SOME-VALUE-1.
            DISPLAY "SOME-VALUE-2:   "  SOME-VALUE-2.
@@ -18,4 +19,5 @@
            DISPLAY "SOME-VALUE-4:   "  SOME-VALUE-4.
            DISPLAY "SOME-VALUE-5:   "  SOME-VALUE-5.
            DISPLAY "SOME-VALUE-6:   "  SOME-VALUE-6.
+           DISPLAY "SOME-VALUE-7:   "  SOME-VALUE-7.
            STOP RUN.
