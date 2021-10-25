15ca9cada2cc09b9f3a844dc0ecf79e3e6659d8b
szaqal <malczyk.pawel@gmail.com>
2021-10-25 09:52:32 +0200

    Misc

---
+++ Cobol/perform2.cob
@@ -0,0 +1,15 @@
+        IDENTIFICATION DIVISION.
+        Program-ID. Init.
+      *  Author. Pawel.
+
+        DATA DIVISION.
+        WORKING-STORAGE SECTION.
+
+        PROCEDURE DIVISION.
+
+        PERFORM 100-TEST.
+
+        100-TEST.
+           DISPLAY "HELLO".
+
+        STOP RUN.            
