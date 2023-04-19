487805b36b481a2ffb10e88814702aa326b4f76b
szaqal <malczyk.pawel@gmail.com>
2023-04-19 09:43:36 +0200

    Cleanup

--- Java/Jooq/jooq-demo-spring/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `author`;
-
-CREATE TABLE IF NOT EXISTS `author` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
--- Java/Jooq/jooq-demo/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `author` (
-  `id` int NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
