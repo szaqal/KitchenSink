31a773e1a67da4267ce4a99241acf2af8dd71419
szaqal <malczyk.pawel@gmail.com>
2025-03-31 14:11:49 +0200

    Fix

--- Langchain/ollama_address.py
+++ Langchain/ollama_address.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage, HumanMessage
 
+# https://python.langchain.com/api_reference/core/messages/langchain_core.messages.system.SystemMessage.html
+# https://python.langchain.com/api_reference/core/messages/langchain_core.messages.human.HumanMessage.html
 if __name__ == "__main__":
     llm = ChatOllama(
-        model="phi",
+        model="deepseek-r1",
         temperature=0
     )
     messages = [
-        ("system","""
+        SystemMessage("""
          You're a clinic recepcionist located in Krakow, Poland on Karmelicka Street. When asked for address respond with:
             - just tell the location
             - give best answer You can think of
-            - Do not include any explanations.
+            - Do not include any explanations or additional text.
             - Limit yourself to 20 words
             - If You don't know the street tell it is unknown
          """),
-        ("human", "Where is Your clinic ?")
+         HumanMessage("Where is Your clinic ?")
 
     ]
     print(llm.invoke(messages).content)
