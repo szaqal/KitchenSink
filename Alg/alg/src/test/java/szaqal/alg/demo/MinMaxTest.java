86785c5af389062a99b486c06c4512e2c0626ced
Pawel Malczyk <malczyk.pawel@gmail.com>
2025-05-10 12:18:41 +0200

    MinMax

--- Alg/alg/src/test/java/szaqal/alg/demo/MinMaxTest.java
+++ Alg/alg/src/test/java/szaqal/alg/demo/MinMaxTest.java
@@ -8,7 +8,12 @@ public class MinMaxTest {
 
     @Test
     public void test() {
-        MinMaxSum.miniMaxSum(List.of(1,2,3,4,5));
-        MinMaxSum.miniMaxSum(List.of(769082435,210437958,673982045,375809214,380564127));
+
+        //MinMaxSum.miniMaxSum(List.of(1,2,3,4,5));
+        //1640793344 219943782
+        //MinMaxSum.miniMaxSum(List.of(769082435,210437958,673982045,375809214,380564127));
+
+        //TODO:
+        MinMaxSum.miniMaxSum(List.of(5,5,5,5,5));
     }
 }
