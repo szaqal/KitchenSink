5dc8f6a2d2bea6c0e9dbb049d79a57c91f7b551d
szaqal <malczyk.pawel@gmail.com>
2026-03-05 10:58:55 +0100

    Dummy

---
+++ Alg/alg/src/main/java/szaqal/alg/demo/Dummy.java
@@ -0,0 +1,41 @@
+package szaqal.alg.demo;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Dummy {
+
+  public static List<Character> characterDups(String aa) {
+    char[] chars = aa.toCharArray(); //No Arrays.stream(char[] ) exists
+    List<Character> characters = new ArrayList<>();
+    for (var chr : chars) {
+      characters.add(chr);
+    }
+
+    return characters.stream()
+        .collect(Collectors.groupingBy(x -> x, Collectors.counting()))
+        .entrySet()
+        .stream()
+        .filter(x -> x.getValue() > 1)
+        .map(Map.Entry::getKey).toList();
+  }
+
+  public static boolean isAnagram(String str1, String str2) {
+
+    Set<Character> chars1 = new HashSet<>();
+    Set<Character> chars2 = new HashSet<>();
+    if(str1.length() != str2.length()) {
+      return false;
+    }
+
+    char[] str1Arr = str1.toCharArray();
+    char[] str2Arr = str2.toCharArray();
+
+    for (int i=0;i < str1.length();i++) {
+      chars1.add(str1Arr[i]);
+      chars2.add(str2Arr[i]);
+    }
+    return chars1.equals(chars2);
+
+  }
+}
---
+++ Alg/alg/src/test/java/szaqal/alg/demo/DummyTest.java
@@ -0,0 +1,31 @@
+package szaqal.alg.demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyTest {
+
+  @Test
+  public void testCharDups() {
+    List<Character> characters = Dummy.characterDups("AAABBC");
+    assertEquals('A', characters.get(0));
+    assertEquals('B', characters.get(1));
+  }
+
+  @Test
+  public void testAnagram() {
+    assertTrue(Dummy.isAnagram("word", "wrdo"));
+    assertTrue(Dummy.isAnagram("mary", "army"));
+    assertTrue(Dummy.isAnagram("stop", "tops"));
+    assertTrue(Dummy.isAnagram("boat", "btoa"));
+    assertFalse(Dummy.isAnagram("pure", "in"));
+    assertFalse(Dummy.isAnagram("fill", "fil"));
+    assertFalse(Dummy.isAnagram("b", "bbb"));
+    assertFalse(Dummy.isAnagram("ccc", "ccccccc"));
+    assertTrue(Dummy.isAnagram("a", "a"));
+    assertFalse(Dummy.isAnagram("sleep", "slep"));
+  }
+}
