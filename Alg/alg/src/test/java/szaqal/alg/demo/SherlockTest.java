3b8e681bb4ba00c3da3e30765de35c7566e4da2d
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-03-06 10:20:25 +0100

    Hackerrank

--- Alg/alg/src/test/java/szaqal/alg/demo/SherlockTest.java
+++ Alg/alg/src/test/java/szaqal/alg/demo/SherlockTest.java
@@ -1,9 +1,14 @@
 package szaqal.alg.demo;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,7 +20,13 @@ public class SherlockTest {
         assertEquals(expected, Sherlock.isValid(input));
     }
 
-    private static List<Arguments> argumentsList () {
+    @Test
+    public void test2() throws Exception {
+        String longString = Files.readString(Paths.get("src/test/resources/sherlock.txt"));
+        assertEquals("YES", Sherlock.isValid(longString));
+    }
+
+    private static List<Arguments> argumentsList() {
         return List.of(
                 Arguments.of("abcdefghhgfedecba", "YES"),
                 Arguments.of("abc", "YES"),
