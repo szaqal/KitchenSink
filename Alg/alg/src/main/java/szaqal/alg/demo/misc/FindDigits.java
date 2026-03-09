d1440d6d87ce9e40a241e0a13a28556b61144cf2
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-03-09 14:29:03 +0100

    Misc

--- Alg/alg/src/main/java/szaqal/alg/demo/misc/FindDigits.java
+++ Alg/alg/src/main/java/szaqal/alg/demo/misc/FindDigits.java
@@ -1,4 +1,4 @@
-package szaqal.alg.demo;
+package szaqal.alg.demo.misc;
 
 /**
  * https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
@@ -8,10 +8,11 @@ public class FindDigits {
     public static int findDigits(int n) {
         int found = 0;
 
+
         char[] charArray = String.valueOf(n).toCharArray();
         for (char x : charArray) {
-
-            int i = Integer.parseInt(String.valueOf(x));
+            //Integer.parseInt(String.valueOf(x)) is expensive
+            int i = Character.getNumericValue(x);
             if (i!=0 && n % i == 0) {
                 found++;
             }
