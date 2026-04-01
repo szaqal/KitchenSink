790f5512f09d145f099f5058082a1e157be31bd0
Pawel Malczyk <malczyk.pawel@gmail.com>
2026-04-01 16:36:58 +0200

    Misc

--- programming/Kotlin/Kotlin/src/main/kotlin/org/kotlin/demo/KotlinDemo.kt
+++ programming/Kotlin/Kotlin/src/main/kotlin/org/kotlin/demo/KotlinDemo.kt
@@ -6,8 +6,8 @@ class KotlinDemo {
         return "ELO";
     }
 
-    fun add(a: Int, b:Int): Int {
-        return a+b;
+    fun add(a: Int, b: Int): Int {
+        return a + b;
     }
 
     fun strTempl(a: Int): String {
@@ -17,10 +17,39 @@ class KotlinDemo {
     fun immutability(): Int {
         //val x=5
         var x = 5
-        x=3
-        val c:Int
+        x = 3
+        val c: Int
         c = x
         return c
     }
 
- }
+    fun maxOf(a: Int, b: Int) = if (a > b) a else b;
+
+
+    fun loop1(): Int {
+        val items = listOf(1, 2, 3);
+        for (i in items) {
+            print(i)
+        }
+        return items.sum()
+    }
+
+
+    fun desc(o: Any): String =
+        when (o) {
+            1 -> "one"
+            else -> "Unknown"
+        }
+
+    //RANGES
+    fun inRange(a:Int) = a in 1..10
+
+    fun filter(a: List<String>):Int {
+        return a
+            .filter { it.startsWith("x") }
+            .map { it.uppercase() }
+            .count()
+    }
+
+    fun optional(str:String):Int? = str.toIntOrNull();
+}
