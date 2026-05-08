982a9fc1ba735ffffcf601171665ad3dba9f9d66
szaqal <malczyk.pawel@gmail.com>
2026-05-08 15:05:46 +0200

    Kotlin (#111)
    Kotlin misc

---
+++ programming/Kotlin/Kotlin/src/main/kotlin/org/kotlin/demo/KotlinDemo.kt
@@ -0,0 +1,57 @@
+package org.kotlin.demo
+
+class KotlinDemo {
+    fun say(): String {
+        print("Exec")
+        return "ELO";
+    }
+
+    fun add(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    fun strTempl(a: Int): String {
+        return "Given ${a}"
+    }
+
+    fun immutability(): Int {
+        //val x=5
+        var x = 5
+        x = 3
+        val c: Int
+        c = x
+        return c
+    }
+
+    fun maxOf(a: Int, b: Int) = if (a > b) a else b;
+
+
+    fun loop1(): Int {
+        val items = listOf(1, 2, 3);
+        for (i in items) {
+            print(i)
+        }
+        return items.sum()
+    }
+
+
+    fun desc(o: Any): String =
+        when (o) {
+            1 -> "one"
+            else -> "Unknown"
+        }
+
+    //RANGES
+    fun inRange(a:Int) = a in 1..10
+
+    fun filter(a: List<String>):Int {
+        return a
+            .filter { it.startsWith("x") }
+            .map { it.uppercase() }
+            .count()
+    }
+
+    fun optional(str:String):Int? = str.toIntOrNull();
+
+    fun equality():Boolean = 2==2 && 2===2;
+}
---
+++ programming/Kotlin/Kotlin/src/test/kotlin/org/kotlin/demo/KotlinDemoTest.kt
@@ -0,0 +1,31 @@
+package org.kotlin.demo
+
+import org.junit.jupiter.api.Assertions.assertNull
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class KotlinDemoTest {
+    @Test
+    fun aaa() {
+        // TODO: ==, != - equality operators  (equals)
+        // TODO: ===, !== - referential equality operators.
+
+
+
+        val kotlinDemo = KotlinDemo()
+        assertEquals("ELO", kotlinDemo.say())
+        assertEquals(3, kotlinDemo.add(1,2))
+        assertEquals("Given 2", kotlinDemo.strTempl(2))
+        assertEquals(3, kotlinDemo.immutability())
+        assertEquals(2, kotlinDemo.maxOf(1,2))
+        assertEquals(6, kotlinDemo.loop1())
+        assertEquals("one", kotlinDemo.desc(1))
+        assertEquals(1, kotlinDemo.filter(listOf("x", "ax", "abc")))
+
+        assertNull(kotlinDemo.optional("d"))
+        assertNotNull(kotlinDemo.optional("1"))
+        assertTrue(kotlinDemo.equality())
+    }
+}
